d58a14b8243306f1dfafa26a0c5c2e1ee1b07669
ichsankurnia <ichsankurniawan51@gmail.com>
2019-09-04 22:17:51 +0700

    adjust code

--- File/2_.py
+++ File/2_.py
@@ -1,7 +1,7 @@
 import csv
 
-""" Membaca File CSV pada Python
-array =[] #array
+""" Membaca File CSV pada Python """
+array = [] #array
 
 with open("MOCK_DATA.csv", "r") as csvfile: #with untuk streaming file, secara otomatis akan close bersihin file saat ga di pake lagi
     csvreader = csv.DictReader(csvfile) #bentuk dictionary
@@ -9,24 +9,26 @@ with open("MOCK_DATA.csv", "r") as csvfile: #with untuk streaming file, secara o
     # for x in csvreader: #buat coment ctrl + /
     #      array.append(x)
     array = list(csvreader)
-    print("total baris : ", csvreader.line_num)
+    print("total baris : ", csvreader.line_num, "\n")
 
 for x in array[:5]: #membatasi cuma membaca 5 data
     # print(x[0] + " - " + x[1]) # dalam list
     print(x["first_name"] + " - " + x["email"]) #dalam dictionary
 
+print()
+
 print(array)
-"""
+
 
 """Menulis File CSV pada python"""
 
-array = [{"nama" : "Mutia", "skill" : "Program", "grade" : 90},
-        {"nama" : "Shafira", "skill" : "Coding", "grade" : 95,},
-        {"nama" : "Chairunisa", "skill" : "Gabut", "grade" : 80,}]
+# array = [{"nama" : "Mutia", "skill" : "Program", "grade" : 90},
+#         {"nama" : "Shafira", "skill" : "Coding", "grade" : 95,},
+#         {"nama" : "Chairunisa", "skill" : "Gabut", "grade" : 80,}]
 
-with open("file.csv", "a") as csvfile:
-        fields = ["nama", "skill", "grade"]
-        writer = csv.DictWriter(csvfile, fieldnames = fields)
+# with open("file.csv", "a") as csvfile:
+#         fields = ["nama", "skill", "grade"]
+#         writer = csv.DictWriter(csvfile, fieldnames = fields)
 
-        writer.writeheader()
-        writer.writerows(array)
+#         writer.writeheader()
+#         writer.writerows(array)
