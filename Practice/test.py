08632ec821154199fb23bd388ad267ad2822dcce
ichsankurnia <ichsankurniawan51@gmail.com>
2019-09-07 23:47:38 +0700

    fix script

--- Practice/test.py
+++ Practice/test.py
@@ -1,11 +1,24 @@
+# def function(kata):
+
+# 	kata = kata[:len(kata)-2]
+	
+# 	return kata
+
 def function(kata):
 
-	kata = kata[:len(kata)-2]
-	
-	return kata
+    s = set()
+    string = []
+
+    for char in kata:
+        if char not in s:
+            s.add(char)
+            string.append(char)
+
+    return ''.join(string)        
+
 
 if __name__ == '__main__':
-	kata = input()
+	kata = input().upper()
 	
 	hasil = function(kata)
 	print(hasil)
