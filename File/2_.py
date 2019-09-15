87339f637394f23325841d107cf2bffca8a2654e
ichsankurnia <ichsankurniawan51@gmail.com>
2019-09-15 22:19:01 +0700

    update scripts

--- File/2_.py
+++ File/2_.py
@@ -3,7 +3,7 @@ import csv
 """ Membaca File CSV pada Python """
 array = [] #array
 
-with open("MOCK_DATA.csv", "r") as csvfile: #with untuk streaming file, secara otomatis akan close bersihin file saat ga di pake lagi
+with open("file.csv", "r") as csvfile: #with untuk streaming file, secara otomatis akan close bersihin file saat ga di pake lagi
     csvreader = csv.DictReader(csvfile) #bentuk dictionary
     # csvreader = csv.reader(csvfile) #bentuk normal
     # for x in csvreader: #buat coment ctrl + /
@@ -11,9 +11,9 @@ with open("MOCK_DATA.csv", "r") as csvfile: #with untuk streaming file, secara o
     array = list(csvreader)
     print("total baris : ", csvreader.line_num, "\n")
 
-for x in array[:5]: #membatasi cuma membaca 5 data
+for x in array[-5:]: #membatasi cuma membaca 5 data
     # print(x[0] + " - " + x[1]) # dalam list
-    print(x["first_name"] + " - " + x["email"]) #dalam dictionary
+    print(x["nama"] + " - " + x["skill"]) #dalam dictionary
 
 print()
 
@@ -22,13 +22,13 @@ print(array)
 
 """Menulis File CSV pada python"""
 
-# array = [{"nama" : "Mutia", "skill" : "Program", "grade" : 90},
-#         {"nama" : "Shafira", "skill" : "Coding", "grade" : 95,},
-#         {"nama" : "Chairunisa", "skill" : "Gabut", "grade" : 80,}]
+array = [{"nama" : "Mutia", "skill" : "Program", "grade" : 90},
+        {"nama" : "Shafira", "skill" : "Coding", "grade" : 95,},
+        {"nama" : "Chairunisa", "skill" : "Gabut", "grade" : 80,}]
 
-# with open("file.csv", "a") as csvfile:
-#         fields = ["nama", "skill", "grade"]
-#         writer = csv.DictWriter(csvfile, fieldnames = fields)
+with open("file.csv", "a") as csvfile:
+        fields = ["nama", "skill", "grade"]
+        writer = csv.DictWriter(csvfile, fieldnames = fields)
 
-#         writer.writeheader()
-#         writer.writerows(array)
+        writer.writeheader()
+        writer.writerows(array)
